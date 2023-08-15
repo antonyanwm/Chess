e82632cb2e6d62bebf41b84885d73d965eff1863
Karlen Nersisyan <karlennersisyan814@gmail.com>
2023-08-16 02:13:55 +0400

    add robots.txt file and refactoring

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import {} from "./module/Board/index.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    // <StrictMode>
-    <App />
-    // </StrictMode>
-);
+//* Styles
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
